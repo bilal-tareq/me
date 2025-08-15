822df4c2fece1433dff6faea4c909d2d8376bb0c
bilal-tareq <bilaltareq2005@gmail.com>
2025-08-15 13:33:57 +0400

    btton

--- scripte/order.js
+++ scripte/order.js
@@ -1,9 +1,17 @@
 const fris = document.getElementById("btn");
 const fri = document.querySelector(".fri");
-
+const sec = document.querySelector(".sec");
 
 
 
 fris.addEventListener("click", function() {
-    fri.style.display = "block";
+    fri.style.display = "none";
+    sec.remove("sec");
+
+    const newElement = document.createElement("a");
+    newElement.textContent = "Go to Home";
+    newElement.getAttribute("href");
+    newElement.setAttribute("href", "https://github.com/bilal-tareq/me");
+    document.body.appendChild(newElement);
+    
 });
